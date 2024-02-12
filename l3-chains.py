bbf693e0886deab8b23eb54d5ccbb2b851337fc2
Luke Byrne <lukebyrnee97@gmail.com>
2024-02-12 13:09:34 +0000

    simple sequential chain

--- l3-chains.py
+++ l3-chains.py
@@ -7,6 +7,7 @@ from langchain.chat_models import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
 from langchain.chains import LLMChain
 import pandas as pd
+from langchain.chains import SimpleSequentialChain
 
 # Setup
 warnings.filterwarnings('ignore')
@@ -15,6 +16,7 @@ llm = ChatOpenAI(temperature=0.0, model=llm_model)
 
 df = pd.read_csv('Data.csv')
 
+#LLMChain
 llm = ChatOpenAI(temperature=0.9, model=llm_model)
 prompt = ChatPromptTemplate.from_template(
     "What is the best name to describe \
@@ -23,3 +25,27 @@ prompt = ChatPromptTemplate.from_template(
 chain = LLMChain(llm=llm, prompt=prompt)
 product = "Queen Size Sheet Set"
 chain.run(product)
+
+#SimpleSequentialChain - 1 in 1 out
+llm = ChatOpenAI(temperature=0.9, model=llm_model)
+
+# prompt template 1
+first_prompt = ChatPromptTemplate.from_template(
+    "What is the best name to describe \
+    a company that makes {product}?"
+)
+
+# Chain 1
+chain_one = LLMChain(llm=llm, prompt=first_prompt)
+
+# prompt template 2
+second_prompt = ChatPromptTemplate.from_template(
+    "Write a 20 words description for the following \
+    company:{company_name}"
+)
+# chain 2
+chain_two = LLMChain(llm=llm, prompt=second_prompt)
+
+overall_simple_chain = SimpleSequentialChain(chains=[chain_one, chain_two],
+                                             verbose=True
+                                            )

6419ef1d65b49ad43bf280dd4f41d37144ad9226
Luke Byrne <lukebyrnee97@gmail.com>
2024-02-12 11:17:06 +0000

    add set_model function for repeatability

--- l1a-openai_generic_prompt.py
+++ l1a-openai_generic_prompt.py
@@ -1,24 +1,13 @@
 import os
+from set_model import llm_model
 from openai import OpenAI
+from dotenv import load_dotenv, find_dotenv
 
 client = OpenAI(api_key=os.environ['OPENAI_API_KEY'])
 
-from dotenv import load_dotenv, find_dotenv
 _ = load_dotenv(find_dotenv()) # read local .env file
 
-# account for deprecation of LLM model
-import datetime
-# Get the current date
-current_date = datetime.datetime.now().date()
-
-# Define the date after which the model should be set to "gpt-3.5-turbo"
-target_date = datetime.date(2024, 6, 12)
-
-# Set the model variable based on the current date
-if current_date > target_date:
-    llm_model = "gpt-3.5-turbo"
-else:
-    llm_model = "gpt-3.5-turbo-0301"
+llm_model = llm_model()
 
 def get_completion(prompt, model=llm_model):
     messages = [{"role": "user", "content": prompt}]
--- l1b-prompt_template_langchain.py
+++ l1b-prompt_template_langchain.py
@@ -1,19 +1,8 @@
-import datetime
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
+from set_model import llm_model
 
-# account for deprecation of LLM model
-# Get the current date
-current_date = datetime.datetime.now().date()
-
-# Define the date after which the model should be set to "gpt-3.5-turbo"
-target_date = datetime.date(2024, 6, 12)
-
-# Set the model variable based on the current date
-if current_date > target_date:
-    llm_model = "gpt-3.5-turbo"
-else:
-    llm_model = "gpt-3.5-turbo-0301"
+llm_model = llm_model()
 
 # To control the randomness and creativity of the generated
 # text by an LLM, use temperature = 0.0

c61a98ffbd3608ef9e04cb45c8c24cc284765bfd
Luke Byrne <lukebyrnee97@gmail.com>
2024-02-12 12:53:50 +0000

    ConversationSummaryBufferMemory

--- l2-memory.py
+++ l2-memory.py
@@ -7,7 +7,8 @@ from langchain.chains import ConversationChain
 from langchain.memory import ConversationBufferMemory
 from langchain.memory import ConversationBufferWindowMemory
 from langchain.memory import ConversationTokenBufferMemory
-from langchain.llms import OpenAI
+from langchain.memory import ConversationSummaryBufferMemory
+
 llm = ChatOpenAI(temperature=0.0, model=llm_model)
 
 _ = load_dotenv(find_dotenv()) # read local .env file
@@ -64,5 +65,31 @@ memory.save_context({"input": "Backpropagation is what?"},
                     {"output": "Beautiful!"})
 memory.save_context({"input": "Chatbots are what?"}, 
                     {"output": "Charming!"})
+
+memory.load_memory_variables({})
+
+# create a long string
+schedule = "There is a meeting at 8am with your product team. \
+You will need your powerpoint presentation prepared. \
+9am-12pm have time to work on your LangChain \
+project which will go quickly because Langchain is such a powerful tool. \
+At Noon, lunch at the italian resturant with a customer who is driving \
+from over an hour away to meet you to understand the latest in AI. \
+Be sure to bring your laptop to show the latest LLM demo."
+
+memory = ConversationSummaryBufferMemory(llm=llm, max_token_limit=100)
+memory.save_context({"input": "Hello"}, {"output": "What's up"})
+memory.save_context({"input": "Not much, just hanging"},
+                    {"output": "Cool"})
+memory.save_context({"input": "What is on the schedule today?"}, 
+                    {"output": f"{schedule}"})
+
+conversation = ConversationChain(
+    llm=llm, 
+    memory = memory,
+    verbose=True
+)
+
+conversation.predict(input="What would be a good demo to show?")
 
 memory.load_memory_variables({})

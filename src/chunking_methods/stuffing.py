4efdc455c18678008204a0a647736a0c8491d220
Luke Byrne <lukebyrnee97@gmail.com>
2024-02-13 15:29:02 +0000

    update stuffing method to work on local machine (as opposed to ipynb)

--- src/chunking_methods/stuffing.py
+++ src/chunking_methods/stuffing.py
@@ -1,71 +1,38 @@
 from dotenv import load_dotenv, find_dotenv
-# Updated imports from langchain-community and langchain-openai
 from langchain.chains import RetrievalQA
 from langchain_openai import ChatOpenAI
 from langchain_community.document_loaders import CSVLoader
-from langchain_community.vectorstores import DocArrayInMemorySearch
-from IPython.display import display, Markdown
-from langchain_openai import OpenAI
+from langchain_community.vectorstores import Chroma
 from langchain.indexes import VectorstoreIndexCreator
 from langchain_openai.embeddings import OpenAIEmbeddings
 import sys
 import os
 
-# Add the parent directory of 'src' to sys.path for local module imports
-sys.path.insert(0, os.path.abspath('../modules/'))
-
-from set_model import llm_model
-
+# Basic Setup
 _ = load_dotenv(find_dotenv()) # read local .env file
+# add the parent directory of 'src' to sys.path for local module imports
+sys.path.insert(0, os.path.abspath('../modules/'))
+from set_model import llm_model
 llm_model = llm_model()
 
+# Load data into vector db
 file = '../../data/OutdoorClothingCatalog_1000.csv'
 loader = CSVLoader(file_path=file)
-
-index = VectorstoreIndexCreator(
-    vectorstore_cls=DocArrayInMemorySearch
-).from_loaders([loader])
-
-query ="Please list all your shirts with sun protection \
-in a table in markdown and summarize each one."
-
-# Step By Step
-
-loader = CSVLoader(file_path=file)
-
-docs = loader.load()
-
-docs[0]
-
-embeddings = OpenAIEmbeddings()
-
-embed = embeddings.embed_query("Hi my name is Harrison")
-
-print(len(embed))
-
-print(embed[:5])
-
-db = DocArrayInMemorySearch.from_documents(
-    docs, 
-    embeddings
+documents = loader.load()
+embedding = OpenAIEmbeddings() # defining embedding
+db = Chroma.from_documents(
+    documents, 
+    embedding
 )
 
+# Run query against the db
 query = "Please suggest a shirt with sunblocking"
-
 docs = db.similarity_search(query)
-
-len(docs)
-
-docs[0]
-
-# hand cranked version
-llm = ChatOpenAI(temperature = 0.0, model=llm_model)
-qdocs = "".join([docs[i].page_content for i in range(len(docs))])
-response = llm.call_as_llm(f"{qdocs} Question: Please list all your \
-shirts with sun protection in a table in markdown and summarize each one.")
-display(Markdown(response))
+print(len(docs))
+print(docs[0])
 
 # langchain version - saves pulling the docs together into a single var as uses vector db
+llm = ChatOpenAI(temperature = 0.0, model=llm_model)
 retriever = db.as_retriever()
 qa_stuff = RetrievalQA.from_chain_type(
     llm=llm, 
@@ -76,13 +43,11 @@ qa_stuff = RetrievalQA.from_chain_type(
 query =  "Please list all your shirts with sun protection in a table \
 in markdown and summarize each one."
 response = qa_stuff.run(query)
-display(Markdown(response))
+print("here")
+print(response)
 
-# shortened langchain version (equivalent to above)
-response = index.query(query, llm=llm)
-
-# customise the index and vector store
+# customise the index and vector store - why?
 index = VectorstoreIndexCreator(
-    vectorstore_cls=DocArrayInMemorySearch,
-    embedding=embeddings,
+    vectorstore_cls=Chroma,
+    embedding=embedding,
 ).from_loaders([loader])

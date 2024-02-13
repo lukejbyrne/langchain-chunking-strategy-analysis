e1f98a1b41906881a9c0adf69539b65ea04f57fb
Luke Byrne <lukebyrnee97@gmail.com>
2024-02-13 10:26:21 +0000

    restructure files

---
+++ tutorials/l6-agents.py
@@ -0,0 +1,97 @@
+from langchain.agents.agent_toolkits import create_python_agent
+from langchain.agents import load_tools, initialize_agent
+from langchain.agents import AgentType
+from langchain.tools.python.tool import PythonREPLTool
+from langchain.python import PythonREPL
+from langchain.chat_models import ChatOpenAI
+import os
+from dotenv import load_dotenv, find_dotenv
+import warnings
+from set_model import llm_model
+from langchain.agents import tool
+from datetime import date
+
+# Using built in LangChain tools: DuckDuckGo search and Wikipedia
+# Defining your own tools
+
+_ = load_dotenv(find_dotenv()) # read local .env file
+warnings.filterwarnings("ignore")
+llm_model = llm_model()
+
+# Note: LLM's do not always produce the same results. When executing the code in your notebook, you may get slightly different answers that those in the video.
+
+# Built-in LangChain tools
+llm = ChatOpenAI(temperature=0, model=llm_model)
+
+tools = load_tools(["llm-math","wikipedia"], llm=llm)
+
+agent= initialize_agent(
+    tools, 
+    llm, 
+    agent=AgentType.CHAT_ZERO_SHOT_REACT_DESCRIPTION,
+    handle_parsing_errors=True,
+    verbose = True)
+
+agent("What is the 25% of 300?")
+
+# Wikipedia example
+question = "Tom M. Mitchell is an American computer scientist \
+and the Founders University Professor at Carnegie Mellon University (CMU)\
+what book did he write?"
+result = agent(question) 
+
+# Python Agent
+agent = create_python_agent(
+    llm,
+    tool=PythonREPLTool(),
+    verbose=True
+)
+
+customer_list = [["Harrison", "Chase"], 
+                 ["Lang", "Chain"],
+                 ["Dolly", "Too"],
+                 ["Elle", "Elem"], 
+                 ["Geoff","Fusion"], 
+                 ["Trance","Former"],
+                 ["Jen","Ayai"]
+                ]
+
+agent.run(f"""Sort these customers by \
+last name and then first name \
+and print the output: {customer_list}""") 
+
+# View detailed outputs of the chains
+import langchain
+langchain.debug=True
+agent.run(f"""Sort these customers by \
+last name and then first name \
+and print the output: {customer_list}""") 
+langchain.debug=False
+
+# Custom tool: tell me the date
+# how agent will know when and how to call this tool via thought string below
+@tool
+def time(text: str) -> str:
+    """Returns todays date, use this for any \
+    questions related to knowing todays date. \
+    The input should always be an empty string, \
+    and this function will always return todays \
+    date - any date mathmatics should occur \
+    outside this function."""
+    return str(date.today())
+
+agent= initialize_agent(
+    tools + [time], 
+    llm, 
+    agent=AgentType.CHAT_ZERO_SHOT_REACT_DESCRIPTION,
+    handle_parsing_errors=True,
+    verbose = True)
+
+# Note:
+# The agent will sometimes come to the wrong conclusion (agents are a work in progress!).
+# If it does, please try running it again.
+
+try:
+    result = agent("whats the date today?") 
+except: 
+    print("exception on external access")
